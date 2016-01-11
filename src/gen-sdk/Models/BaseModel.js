d9340c97a5171ae9a799aa19e6316a4fcfcf3197
zallek <nicolas.fortin37@gmail.com>
2016-01-11 17:02:47 +0100

    Add generated code

---
+++ src/gen-sdk/Models/BaseModel.js
@@ -0,0 +1,30 @@
+/**
+ * BotifyAPILib
+ *
+ * This file was automatically generated for Botify by APIMATIC BETA v2.0 on 01/11/2016
+ */
+
+/**
+ * Creates a instance of BaseModel
+ *
+ * @constructor
+ */
+function BaseModel() {
+	//Maintain a name mapping dictionary
+    this._variableDict= [];
+    this.toJSON = function(){
+        var newDict = {};
+            for(var prop in this){                       
+    		    if(typeof this[prop]!=="function" && prop !== "_variableDict"){                         
+    		        if(prop in this._variableDict){  
+    		            var value = this._variableDict[prop];
+                        newDict[value] = this[prop];
+                    }else{                         
+                        newDict[prop] = this[prop];
+                    }
+                }
+            } 
+        return (newDict);
+    }
+}
+module.exports = BaseModel;
---
+++ src/gen-sdk/Models/UrlsAggsQuery.js
@@ -0,0 +1,57 @@
+
+/**
+ * BotifyAPILib
+ *
+ * This file was automatically generated for Botify by APIMATIC BETA v2.0 on 01/11/2016
+ */
+var BaseModel = require("./BaseModel");
+/**
+ * Creates a instance of UrlsAggsQuery
+ *
+ * @constructor
+ */
+function UrlsAggsQuery() {
+    this.filters = null;     
+    this.aggs = null;     
+}
+
+UrlsAggsQuery.prototype = new BaseModel();
+UrlsAggsQuery.prototype.constructor = BaseModel;
+
+/**
+ * TODO: Write general description for this method
+ *
+ * @return {mixed|null}
+ */
+UrlsAggsQuery.prototype.getFilters = function() {
+    return this.filters;
+};
+
+/**
+ * Setter for Filters
+ * 
+ * @param {mixed|null} value 
+ */
+UrlsAggsQuery.prototype.setFilters = function(value) {
+    this.filters = value;
+};
+
+/**
+ * TODO: Write general description for this method
+ *
+ * @return {array|null}
+ */
+UrlsAggsQuery.prototype.getAggs = function() {
+    return this.aggs;
+};
+
+/**
+ * Setter for Aggs
+ * 
+ * @param {array|null} value 
+ */
+UrlsAggsQuery.prototype.setAggs = function(value) {
+    this.aggs = value;
+};
+
+module.exports = UrlsAggsQuery;
